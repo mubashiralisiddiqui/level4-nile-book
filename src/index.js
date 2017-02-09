37a28050f31a0bacd3d03ee9f9e680eee9620706
mubashiralisiddiqui <mubashir22ali@gmail.com>
2017-02-10 01:31:09 +0500

    react tooltip app

--- src/index.js
+++ src/index.js
@@ -3,53 +3,81 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-class Link extends React.Component{
-  render() {
-    var url='/' + this.props.label.toLowerCase().trim().replace(' ', '-')
-    return (
-      <li>
-        <a className="list" href={url}>
-      {this.props.label}
-      </a>
-      </li>
-      
-    )
+
+class ToolTip extends React.Component {
+toggle() {
+const tooltipNode = ReactDOM.findDOMNode(this)
+this.setState({
+      opacity: !this.state.opacity,
+      top: tooltipNode.offsetTop,
+      left: tooltipNode.offsetLeft
+    });
+}
+
+  constructor(props) {
+   super(props)
+   this.toggle = this.toggle.bind(this)
+    this.state = {opacity: false}
   }
+
+  render() {
+const style = {
+zIndex: (this.state.opacity) ? 1000 : -1000,
+opacity: +this.state.opacity,
+top: (this.state.top||0) + 20,
+left: (this.state.left||0) -30}
+
+return (
+<div style={{display: 'inline'}}>
+<span style={{color: 'blue'}}
+onMouseEnter={this.toggle} 
+onMouseOut={this.toggle}>
+{this.props.children} 
+</span>
+<div className="tooltip bottom"
+style={style} 
+role="tooltip">
+<div className="tooltip-arrow"></div>
+<div className="tooltip-inner">
+{this.props.text} 
+</div>
+</div>
+</div>
+)
 }
 
-
-
-class Menu extends React.Component{
-  render(){
-    let menus = ['Home',
-      'About',
-      'Services',
-      'Portfolio',
-      'Contact-us.'];
-    return (
-      <div className="header">
-        
-        <h1 className="text-right"> LEVEL ONE PROJECT </h1>
-        <ul >
-        {menus.map(function(v,i){
-          return(
-          //  <ulclassName="box" key={i}>
-             <Link label={v}>
-             </Link>
-            // </div>
-           )
-        })}
-        </ul>
-      </div>
-    )
-}
 }
 
+ReactDOM.render(
+  <div>
+  
+  <ToolTip  
+  text = 'Master Express.js-The Node.js Framework For Your Web Development' >
+    
+   
+    Pro Express.js'
+  </ToolTip>,
 
-     
-      
-  ReactDOM.render(
-        <Menu/>,
-        document.getElementById("root")
-      );
+  <ToolTip text='Practical Node.js: Building Real-World Scalable Web Apps'>
+    
+    Practical Node.js
+  </ToolTip>,
+  
+  <ToolTip  text='HTTP API server'>
+    'Hapi'
+  </ToolTip>,
+  <ToolTip  text='Release the Kraken!'>
+    'Kraken'
+  </ToolTip>,
+  <ToolTip  text='Sail away'>
+    'Kraken'
+  </ToolTip>,
+   <ToolTip  text='IBM of frameworks'>
+    'Loopback'
+  </ToolTip>,
+  
+
+
+</div>, document.getElementById('root'));
+
 
